607bd59176e861ece0c8d6235d5f93c7ec00eee6
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-07-03 22:13:19 -0300

    Init project from example

---
+++ app/src/androidTest/java/dm/feevale/br/tiposinterface/TelaEntrada.java
@@ -0,0 +1,75 @@
+package dm.feevale.br.tiposinterface;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class TelaEntrada extends Activity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.tela_entrada );
+    }
+
+    public void volta( View v ) {
+        finish();
+    }
+
+    public void voltaComDados( View v ) {
+
+
+        EditText nome = (EditText) findViewById( R.id.edtNome );
+        EditText senha = (EditText) findViewById( R.id.edtSenha );
+
+        String dsNome = nome.getText().toString();
+        String dsSenha = senha.getText().toString();
+
+        if( dsNome.trim().length() == 0 ) {
+
+            new AlertDialog.Builder( this )
+                    .setTitle( R.string.ttl_atencao )
+                    .setMessage( R.string.msg_preencha_nome )
+                    .setPositiveButton( "OK", null )
+                    .show();
+            return;
+        }
+
+        if( dsSenha.trim().length() == 0 ) {
+
+            new AlertDialog.Builder( this )
+                    .setTitle( R.string.ttl_atencao )
+                    .setMessage( R.string.msg_preencha_senha )
+                    .setPositiveButton( "OK", null )
+                    .show();
+            return;
+        }
+
+        Intent dadosVolta = new Intent();
+        dadosVolta.putExtra( "nome", dsNome );
+        dadosVolta.putExtra( "senha", dsSenha );
+        setResult( RESULT_OK, dadosVolta );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/dm/feevale/br/tiposinterface/InterfacePrincipal.java
@@ -0,0 +1,112 @@
+package dm.feevale.br.tiposinterface;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class InterfacePrincipal extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface_principal);
+    }
+
+    public void executaLogin( View v ) {
+
+        EditText nome = (EditText) findViewById( R.id.edtNome );
+        EditText senha = (EditText) findViewById( R.id.edtSenha );
+
+        if( nome.getText().toString().equals( "malu" ) ) {
+
+            if( senha.getText().toString().equals( "111" ) ) {
+                Toast.makeText( this,
+                        getText( R.string.msg_bemvinda ) + nome.getText().toString(),
+                        Toast.LENGTH_LONG ).show();
+                return;
+            }
+        }
+
+/*
+        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+        builder.setTitle( "Atenção" );
+        builder.setMessage( "Usuário ou Senha inválida" );
+        builder.setPositiveButton( "OK", null );
+        builder.show();
+*/
+        new AlertDialog.Builder( this )
+            .setTitle( "Atenção" )
+            .setMessage( "Usuário ou Senha inválida" )
+            .setPositiveButton( "OK", null )
+            .show();
+
+    }
+
+    public void vai( View v ) {
+        setContentView(R.layout.tela_dois);
+    }
+
+    public void volta( View v ) {
+        setContentView(R.layout.activity_interface_principal);
+    }
+
+    public void trocaActivity( View v ) {
+
+        Intent i = new Intent( this, TelaEntrada.class );
+        startActivity( i );
+    }
+
+    public void buscaCampos( View v ) {
+
+        Intent i = new Intent( this, TelaEntrada.class );
+        startActivityForResult( intent, 1 );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if( requestCode == 1 ) {
+
+            if( resultCode == RESULT_OK ) {
+
+                String dsNome = data.getExtras().getString( "nome" );
+                String dsSenha = data.getExtras().getString( "senha" );
+
+                EditText nome = (EditText) findViewById( R.id.edtNome );
+                EditText senha = (EditText) findViewById( R.id.edtSenha );
+
+                nome.setText( dsNome );
+                senha.setText( dsSenha );
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
